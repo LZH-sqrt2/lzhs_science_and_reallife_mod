e6d1b2be4adc37cdba3f127e76f33fa971ef361d
LZH <142026247+LZH-sqrt2@users.noreply.github.com>
2024-09-01 16:22:03 +0800

    Upgrade to 1.20.6

--- src/main/java/lzh/lzhs_science_and_reallife_mod/main/items/itemBase/IngotItem.java
+++
@@ -1,9 +0,0 @@
-package lzh.lzhs_science_and_reallife_mod.main.items.itemBase;
-
-import net.minecraft.world.item.Item;
-
-public class IngotItem extends Item {
-    public IngotItem(Properties pProperties) {
-        super(pProperties.stacksTo(64).fireResistant());
-    }
-}
